537e18798dc2cb7cf66c029e856968d7cdb5902b
Anuj Kumar <anujkrs1724@gmail.com>
2026-05-31 18:26:15 +0530

    complete 5 days

---
+++ day5'3'.py
@@ -0,0 +1,14 @@
+dict={
+    "name":"anuj",
+    "cgpa":"8.9",
+    "sub":["math,english,science"]
+}
+print(dict)
+print(dict.get("sub"))
+print(dict.keys())
+print(dict.values())
+print(dict.items())
+dict.update({"city":"chhapra"})
+print(dict)
+
+
