215d93af67b9a2d3e4090998f3d0c8804fc8b813
Anuj Kumar <anujkrs1724@gmail.com>
2026-06-03 19:01:41 +0530

    day6'2'

---
+++ day6'2'.py
@@ -0,0 +1,13 @@
+class student():
+  def __init__(self,name,age,cgpa):
+    self.name=name
+    self.age=age
+    self.cgpa=cgpa
+  
+  def get_cgpa(self):
+    return self.age
+
+s1=student("anuj",22,9.0)
+s2=student("adarsh",20,9.8)
+
+print(f"{s1.name} is  {s1.get_cgpa()} year old")
